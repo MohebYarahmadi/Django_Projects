a4e2fdad360ad87dcbac3b3ad4176c749c29aed1
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-14 22:19:43 +0330

    Basic Templates added for Blog plus Statis files

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,5 +7,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post-list'),
-    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:pk>/', views.post_detail, name='post-detail'),
 ]
