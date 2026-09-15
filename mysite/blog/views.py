0e60eb96a97a9930b00c693338eb903f588f3adc
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-15 09:35:29 +0330

    Basic CBV Class Based View

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,5 @@
+from django.views.generic import ListView
+
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
@@ -5,21 +7,21 @@ from django.http import Http404
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -31,3 +33,13 @@ def post_detail(request, year, month, day, post):
         published_at__day=day
     )
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
