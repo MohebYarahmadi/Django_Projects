a49381de95ddefdb10f862d4c4761004c5e3070b
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-15 15:34:31 +0330

    Show tags on post list view

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+from taggit.managers import TaggableManager
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -32,6 +34,8 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='blog_posts'
     )
+
+    tags = TaggableManager()
 
     # Managers
     objects = models.Manager()  # the default manager
--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Ex apps
+    'taggit',
     'django_extensions',    # Use `shell_plus`
 
     # My Apps
