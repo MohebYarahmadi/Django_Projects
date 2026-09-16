9ef6785cf3e94dcd3e997470fd14362d8a74bfe0
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-16 18:16:48 +0330

    Replace builtin login and logout by our custom style

--- bookmark/account/urls.py
+++ bookmark/account/urls.py
@@ -1,8 +1,14 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # Our pervious custom login form and view
+    # path('login/', views.user_login, name='login'),
+
+    # Built-in
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
