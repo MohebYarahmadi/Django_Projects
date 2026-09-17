2806119d3a5804bd77fd0e61a09707516c287f28
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-17 16:31:37 +0330

    Custom Authentication Backend Added

--- bookmark/config/settings.py
+++ bookmark/config/settings.py
@@ -75,6 +75,12 @@ DATABASES = {
 }
 
 
+# Custom Authentication Backends
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
