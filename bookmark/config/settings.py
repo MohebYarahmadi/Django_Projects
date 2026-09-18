8a74be38978eec683e52152cdfe762c372a1ab8b
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-18 22:25:29 +0330

    Adding Thumbnail - Fix slugify to have default value if null returned

--- bookmark/config/settings.py
+++ bookmark/config/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     # Ex apps
     'social_django',
     'django_extensions',    # Use `shell_plus` and `runserver_plus`
+    'easy_thumbnails',
 
     # Our apps
     'images.apps.ImagesConfig',
--- bookmark/images/models.py
+++ bookmark/images/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
+from django.urls import reverse
+import uuid
 
 
 class Image(models.Model):
@@ -23,13 +25,15 @@ class Image(models.Model):
         ]
         ordering = ['-created_at']
 
-    def save(self, *arg, **kwargs):
+    def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-        super().save(*arg, **kwargs)
+            if not self.slug:                          # <-- safety net
+                self.slug = f'image-{uuid.uuid4().hex[:8]}'
+            super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('image:detail', args=[self.id, seld.slug])
+        return reverse('images:detail', args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
