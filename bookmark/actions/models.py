8dc8a48e8389a62148f350b6683eacd63ea44a86
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-19 21:13:21 +0330

    Action model to store user activities created

--- bookmark/actions/models.py
+++ bookmark/actions/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
+
 from django.db import models
 
-# Create your models here.
+
+class Action(models.Model):
+    verb = models.CharField(max_lenght=225)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Relations
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='actions')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created_at']),
+        ]
+        ordering = ['-created_at']
