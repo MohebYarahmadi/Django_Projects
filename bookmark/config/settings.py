e37505a98e10fc26405f2b23d0f43b7027d2228e
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-19 18:53:49 +0330

    fix bug - url name

--- bookmark/config/settings.py
+++ bookmark/config/settings.py
@@ -163,5 +163,5 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET')
 from django.urls import reverse_lazy
 
 ABSOLUTE_URL_OVERRIDES = {
-    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+    'auth.user': lambda u: reverse_lazy('user-detail', args=[u.username])
 }
