4a5728171bbe32ab60a80e56f7fdbabaad1c1890
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-19 22:18:31 +0330

    Improve shortcut function to prevent similar actions

--- bookmark/actions/utils.py
+++ bookmark/actions/utils.py
@@ -1,4 +1,6 @@
+import datetime
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 
 from .models import Action
 
@@ -10,5 +12,17 @@ def create_action(user, verb, target=None):
     You can use this function anywhere in your code as a shortcut to add new actions to
         the activity stream.
     """
-    action = Action(user=user, verb=verb, target=target)
-    action.save()
+
+    # Check for any similar action made in the last minute
+    now = timezone.now()
+    last_minute = now - datetime.timedelta(seconds=60)
+    similar_actions = Action.objects.filter(user_id=user.id, verb=verb, created_at__gte=last_minute)
+    if target:
+        target_ct = ContentType.objects.get_for_model(target)
+        similar_actions = similar_actions.filter(target_ct=target_ct, target_id=target.id)
+    if not similar_actions:
+        # no existing actions found
+        action = Action(user=user, verb=verb, target=target)
+        action.save()
+        return True
+    return False
