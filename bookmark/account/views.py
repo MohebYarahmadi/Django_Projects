e1ef44cdfb7cf1f9803a4b2151856087f4923b2e
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-20 09:07:54 +0330

    Optiizing QuerySet that involve related objects

--- bookmark/account/views.py
+++ bookmark/account/views.py
@@ -99,7 +99,7 @@ def dashboard(request):
     if following_ids:
         # If user is followig others, retrieve oly their actions
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
 
     context = {
         'actions': actions,
