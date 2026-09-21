a12c1283a1a3ed0cd4010e2dcdc40ec25b7ea459
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-21 11:36:59 +0330

    Cart class

--- myshop/config/settings.py
+++ myshop/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My Apps
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
 ]
 
@@ -126,3 +127,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Session
+CART_SESSION_ID = 'cart'
