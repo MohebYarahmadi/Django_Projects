a1c97bdf0325fd29425717f56e65807779c2dde4
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-21 13:32:27 +0330

    Cart class ready to use

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -18,6 +18,29 @@ class Cart:
         self.cart = cart
 
 
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products from the database
+        """
+        product_ids = self.cart.keys()
+        # Get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+
     def add(self, product, quantity=1, override_quantity=False):
         """
         Add a product to the cart or update its quantity.
@@ -38,3 +61,26 @@ class Cart:
     def save(self):
         # Mark the session as 'modified' to make sure it gets saved
         self.session.modified = True
+
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+
+    def get_total_price(self):
+        return sum(
+            Decimal(item['price']) * item['quantity']
+            for item in self.cart.values()
+        )
+
+
+    def clear(self):
+        # Remove cart from session
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
