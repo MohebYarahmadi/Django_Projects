f92e1459ea74aff19d09de73bc4fca5a5c3a7830
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-21 15:17:00 +0330

    Add to Cart BTN View

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -26,8 +26,10 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     template_name = 'shop/product/detail.html'
     product = get_object_or_404(Product, id=id, slug=slug, is_available=True)
+    cart_product_form = CartAddProductForm()
     context = {
         'product': product,
+        'cart_product_form': cart_product_form,
     }
-    
+
     return render(request, template_name, context=context)
