bb4d8ace5a0e99fcd0ebe52864365ec8b764da02
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-22 10:04:39 +0330

    Order Form and view added

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,31 @@
+from cart.cart import Cart
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import OrderCreateForm
+from .models import OrderItem
+
+
+def order_create(request):
+    template_name = 'orders/order/created.html'
+    cart = Cart(request)
+    if request.method == 'POST':
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+            # Clear the cart
+            cart.clear()
+            return render(request, template_name, { 'order': order })
+    else:
+        form = OrderCreateForm()
+
+    context = {
+        'cart': cart,
+        'form': form,
+    }
+    return render(request, template_name, context=context)
