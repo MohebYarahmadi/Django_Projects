d5ec93ab52fee70ebb67929732633f5bc99718e8
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-24 09:43:35 +0330

    Fix Celery Configuration

--- myshop/config/celery.py
+++ myshop/config/celery.py
@@ -3,8 +3,8 @@ from celery import Celery
 
 
 # set the default Django settings module for the 'celery' program
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('myshop')
+app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
