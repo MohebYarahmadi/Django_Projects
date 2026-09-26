f1e9f2c05a959a9024e5829a148c12256497006c
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-26 13:28:22 +0330

    Coupons added to order and payment not gateway

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,5 +1,9 @@
 from django.conf import settings
 from django.db import models
+from decimal import Decimal
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from coupons.models import Coupon
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -13,6 +17,9 @@ class Order(models.Model):
     stripe_id = models.CharField(max_length=250, blank=True)
     is_paid = models.BooleanField(default=False)
 
+    coupon = models.ForeignKey(Coupon, null=True, blank=True, on_delete=models.SET_NULL, related_name='orders')
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+
     class Meta:
         ordering = ['-created_at']
         indexes = [
@@ -20,7 +27,8 @@ class Order(models.Model):
         ]
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = self.get_total_const_before_discount()
+        return total_cost - self.get_discount()
 
     def get_stripe_url(self):
         if not self.stripe_id:
@@ -33,6 +41,15 @@ class Order(models.Model):
             # stripe path for real payment
             path = '/'
         return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+
+    def get_total_const_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_const_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
 
     def __str__(self):
         return f'Order {self.id}'
