c56031b5423c580cad4700bc19429bd605400c45
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-26 11:02:31 +0330

    Coupon added to cart class

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart:
@@ -16,6 +17,8 @@ class Cart:
             # Save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
 
 
     def __iter__(self):
@@ -39,6 +42,16 @@ class Cart:
         Count all items in the cart.
         """
         return sum(item['quantity'] for item in self.cart.values())
+
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
 
 
     def add(self, product, quantity=1, override_quantity=False):
@@ -84,3 +97,13 @@ class Cart:
         # Remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
