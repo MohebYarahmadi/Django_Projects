496e6b9c58ab0df208343ae0df239095b8763776
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-26 11:12:37 +0330

    Coupon Form

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
 
 
 def cart_detail(request):
@@ -13,8 +14,10 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(
             initial={'quantity': item['quantity'], 'override': True}
         )
+    coupon_apply_form = CouponApplyForm()
     context = {
         'cart': cart,
+        'coupon_apply_form': coupon_apply_form,
     }
     return render(request, template_name, context=context)
 
